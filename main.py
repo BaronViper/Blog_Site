9786c02aaffff1d5ba20dbfcab37e9f5bbec22be
JeremyS-T <jtanrev@gmail.com>
2023-03-27 15:28:19 +0800

    Initial Commit.

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('generic.html')
+
+
+@app.route('/elements')
+def elements():
+    return render_template('elements.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
