ef673c1275397091361bf2b597f58b033f2300af
JeremyS-T <jtanrev@gmail.com>
2023-04-21 16:32:16 +0800

    Refined Login and Logout, added authentication, CRUD for blog, subjects for posts, error handling, dynamic navbar, search function, pagination, detailed queries, fixed validators, removed reference page from navbar

--- forms.py
+++ forms.py
@@ -1,7 +1,56 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, EmailField, URLField, SelectField
+from wtforms.validators import DataRequired, Length, URL
+
+
+class SearchForm(FlaskForm):
+    searched = StringField('Searched', validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 class LoginForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+
+class BlogForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired(message='Title required'), Length(min=3, max=40,
+                                                                                            message='Title must be 40 characters or less')])
+    subject = SelectField('- Person Focused On -',
+                          validators=[DataRequired(message='Subject required. If no subject available in '
+                                                           'drop-down, add one!')], choices=[("", "- "
+                                                                                                  "Subject "
+                                                                                                  "Focused "
+                                                                                                  "On -")], default="")
+    quote = StringField('quote', validators=[DataRequired(message='Quote required'),
+                                             Length(min=3, max=500, message="Quote too long!")])
+    quote_author = StringField('quote_author',
+                               validators=[DataRequired(message='Quote Author required'), Length(min=3, max=500)])
+    subtitle = StringField('subtitle', validators=[DataRequired(message='Description of blog post required'),
+                                                   Length(min=10, message='Description of blog post too short!')])
+    image = URLField('image', validators=[DataRequired(message='Image URL required'),
+                                          URL(require_tld=False, message='Invalid Image URL')])
+    message = StringField('message', validators=[DataRequired(message='Blog post body required'),
+                                                 Length(min=10, message='Blog post body too short')])
+    submit = SubmitField('Post')
+
+
+class MissionaryForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired(message='Name required'), Length(min=3, max=40)])
+    location = StringField('location', validators=[DataRequired(message='Location required')])
+    quote = StringField('quote', validators=[DataRequired(message='Quote required'), Length(min=3, max=500)])
+    subtitle = StringField('subtitle',
+                           validators=[DataRequired(message='Brief description of biography required'), Length(min=10)])
+    birth = StringField('birth', validators=[DataRequired(message='Year of birth required (XXXX) if unknown')])
+    death = StringField('death', validators=[DataRequired(message='Year of death required (XXXX) if unknown')])
+    image = URLField('image', validators=[DataRequired(message='Image URL required'),
+                                          URL(require_tld=False, message='Invalid Image URL')])
+    message = StringField('message', validators=[DataRequired(message='Biography required'),
+                                                 Length(min=10, message='Biography too short')])
+    submit = SubmitField('Add')
+
+# class ContactForm(FlaskForm):
+#     name = StringField('name', validators=[DataRequired()])
+#     email = EmailField('email', validators=[DataRequired()])
+#     message = StringField('name', validators=[DataRequired()])
--- main.py
+++ main.py
@@ -1,28 +1,276 @@
-from flask import Flask, render_template
-import forms
+from flask import Flask, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_paginate import Pagination, get_page_parameter
+from sqlalchemy.orm import relationship
+from forms import LoginForm, BlogForm, MissionaryForm, SearchForm
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from datetime import datetime
+import os
+
+
+date = datetime
 
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    posts = relationship("BlogPost", back_populates="author")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="posts")
+
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+
+    subject_id = db.Column(db.Integer, db.ForeignKey("missionaries.id"))
+    subject = relationship("Missionaries", back_populates="blogs")
+
+    quote = db.Column(db.String(250), nullable=False)
+    quote_author = db.Column(db.String(250), nullable=False)
+
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+class Missionaries(db.Model):
+    __tablename__ = "missionaries"
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.Text, nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+
+    quote = db.Column(db.String(250), nullable=False)
+
+    birth = db.Column(db.Integer, nullable=False)
+    death = db.Column(db.Integer, nullable=False)
+    biography = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    blogs = relationship("BlogPost", back_populates="subject")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    per_page = 6
+
+    if not BlogPost.query.all():
+        all_posts = False
+        featured_post = 0
+
+    elif page == 1 and BlogPost.query.all():
+        featured_post = BlogPost.query.order_by(BlogPost.id.desc()).first()
+        all_posts = BlogPost.query.filter(BlogPost.id != featured_post.id).order_by(BlogPost.id.desc()).paginate(
+            page=page, per_page=per_page)
+    else:
+        featured_post = 0
+        all_posts = BlogPost.query.filter(
+            BlogPost.id != BlogPost.query.order_by(BlogPost.id.desc()).first().id).order_by(
+            BlogPost.id.desc()).paginate(
+            page=page, per_page=per_page)
+
+    return render_template('index.html', featured_post=featured_post, all_posts=all_posts)
 
 
-@app.route('/blog')
-def blog():
-    return render_template('generic.html')
+# @app.route('/search/<search>', methods=['GET'])
+# def search(keyword):
+#     pass
 
 
-@app.route('/elements')
-def elements():
-    return render_template('elements.html')
+@app.route('/post/<int:post_id>', methods=['GET'])
+def blog(post_id):
+    target_post = BlogPost.query.filter_by(id=post_id).first()
+    return render_template('post.html', post=target_post)
+
+
+@app.context_processor
+def base():
+    form = SearchForm()
+    return dict(form=form)
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        print(type(BlogPost.query.filter(BlogPost.title.like('%' + form.searched.data + '%'))))
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        posts = BlogPost.query.filter(BlogPost.title.like('%' + form.searched.data + '%')).order_by(
+            BlogPost.title).paginate(
+            page=page, per_page=6)
+
+        return render_template('search.html', form=form, all_posts=posts)
+    return redirect(url_for('home'))
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        if User.query.filter_by(name=name).first():
+            password = form.password.data
+            user = User.query.filter_by(name=name).first()
+            if check_password_hash(user.password, password):
+                login_user(user)
+            else:
+                flash(message='Incorrect Password')
+                return redirect(url_for('login'))
+        else:
+            flash(message='Invalid User')
+            return redirect(url_for('login'))
+        return redirect(url_for('home'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/create', methods=["POST", "GET"])
+@login_required
+def create():
+    form = BlogForm()
+    all_missionaries = db.session.query(Missionaries).all()
+    form.subject.choices += [(missionary.name, missionary.name) for missionary in all_missionaries if
+                             (missionary.name, missionary.name) not in form.subject.choices]
+
+    if form.validate_on_submit():
+        if form.subject.data == "":
+            flash(message="Blog must focus on an already registered person. If no person is found in drop down list, "
+                          "add one!")
+            return redirect(url_for('create'))
+        if BlogPost.query.filter_by(title=form.title.data.title()).first():
+            flash(message="Blog post with this title already exists. Please rename your blog.")
+            return redirect(url_for('create'))
+        selected_missionary = Missionaries.query.filter_by(name=form.subject.data).first()
+        new_post = BlogPost(
+            title=form.title.data.title(),
+            subject=selected_missionary,
+            quote=form.quote.data,
+            quote_author=form.quote_author.data.title(),
+            subtitle=form.subtitle.data,
+            body=form.message.data,
+            img_url=form.image.data,
+            author=current_user,
+            date=date.today().strftime("%B %e, %Y"),
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template('create.html', form=form, mission_list=all_missionaries)
+
+
+@app.route('/edit/<int:post_id>', methods=['POST', "GET"])
+@login_required
+def edit_post(post_id):
+    all_missionaries = db.session.query(Missionaries).all()
+    post = db.session.get(BlogPost, post_id)
+    edit_form = BlogForm(
+        title=post.title,
+        subject=post.subject,
+        quote=post.quote,
+        quote_author=post.quote_author,
+        subtitle=post.subtitle,
+        image=post.img_url,
+        message=post.body
+    )
+
+    edit_form.subject.choices += [(missionary.name, missionary.name) for missionary in all_missionaries if
+                                  (missionary.name, missionary.name) not in edit_form.subject.choices]
+    edit_form.subject.default = post.subject.name
+
+    if edit_form.validate_on_submit():
+        selected_missionary = Missionaries.query.filter_by(name=edit_form.subject.data).first()
+        post.title = edit_form.title.data
+        post.subject = selected_missionary
+        post.quote = edit_form.quote.data
+        post.quote_author = edit_form.quote_author.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.image.data
+        post.body = edit_form.message.data
+        db.session.commit()
+
+        return redirect(url_for('blog', post_id=post_id))
+    return render_template('edit.html', form=edit_form, post=post)
+
+
+@app.route('/add-subject', methods=["POST", "GET"])
+@login_required
+def add_subject():
+    form = MissionaryForm()
+    if form.validate_on_submit():
+        if Missionaries.query.filter_by(name=form.name.data.title()).first():
+            flash(message="Person already exists.")
+            return redirect(url_for('add_missionary'))
+        else:
+            new_post = Missionaries(
+                name=form.name.data.title(),
+                location=form.location.data,
+                quote=form.quote.data,
+                subtitle=form.subtitle.data,
+                birth=form.birth.data,
+                death=form.death.data,
+                biography=form.message.data,
+                img_url=form.image.data,
+            )
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect(url_for("home"))
+    return render_template('add_subject.html', form=form)
+
+
+@app.route('/delete-post/<int:post_id>', methods=["POST", "GET"])
+@login_required
+def delete_post(post_id):
+    db.session.delete(BlogPost.query.filter_by(id=post_id).first())
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.errorhandler(404)
+@app.errorhandler(401)
+def page_not_found(error):
+    format_error = str(error)[0:3]
+    error_message = str(error)[str(error).index(":") + 1:]
+    return render_template('404.html', error=format_error, error_message=error_message)
+
+
+# @app.route('/elements')
+# def elements():
+#     return render_template('elements.html')
 
 
 if __name__ == '__main__':
